94a60904e188e020e7d1aba632ff33d49c988c64
ohad1997 <88004449+ohad1997@users.noreply.github.com>
2024-04-22 15:22:41 +0300

    Add files via upload

--- MATLAB projects/situation_equation_solver.m
+++ MATLAB projects/situation_equation_solver.m
@@ -1,15 +1,16 @@
 clc
 %X is a vector of differentiable funcs
 %A is the situation matix
+global t;
 function solution = solve_situation(A,X)
 numOfRows = size(A,1);
 numOfColoms = size(1,A);
-if(numOfColoms == numOfRows && numOfRows == size(X)) %checking if A is a square matrix
-    I = eye(numOfRows)*a;
-    B = A-I;
-    C = cgs(det(B),0,1e-6);
-    solution = 1;
-else % if the matrix is not a square returns zero
+if(numOfColoms == numOfRows && numOfRows == size(X)) %checking if A is a square matrix and if x is viable
+    [Vec,Val] = eig(A);
+    inv_Vec = inv(Vec);
+    temp = Vec*exp(Val.*t)*inv_Vec;
+    solution = temp*X;
+else % if the matrix is not a square or X isnt viable returns zero
     solution = 0;
 end
 end
